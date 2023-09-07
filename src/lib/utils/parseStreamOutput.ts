4b1c594326dcff21d275120921e9de90c40a7219
Dest1n1 <dest1nis2022@gmail.com>
2023-09-07 14:17:37 +0800

    temp: add temporary console logs

--- src/lib/utils/parseStreamOutput.ts
+++ src/lib/utils/parseStreamOutput.ts
@@ -2,12 +2,32 @@ import type { StreamOutput } from "$lib/types/StreamOutput";
 
 export const parseStreamOutput = (chunk: string): StreamOutput => {
     const trimedChunk = chunk.trim();
+    console.log(trimedChunk)
     if (trimedChunk.startsWith("data:")) {
-        const data = JSON.parse(trimedChunk.slice("data:".length));
-        return {
-            type: "text",
-            data,
+        try {
+            const data = JSON.parse(trimedChunk.slice("data:".length));
+            return {
+                type: "text",
+                data,
+            }
+        } catch (e) {
+            console.error("Error: ", trimedChunk)
+            return {
+                type: "text",
+                data: {
+                    token: {
+                        id: -1,
+                        text: "",
+                        special: false,
+                        logprob: 0
+                    },
+                    generated_text: null,
+                    details: null
+                }
+            }
         }
+
+
     } else if (trimedChunk.startsWith("action:")) {
         const data = JSON.parse(trimedChunk.slice("action:".length));
         return {
